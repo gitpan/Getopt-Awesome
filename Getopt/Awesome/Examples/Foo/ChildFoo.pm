13b8abcabcac5d5c7b1ff42dc439db2fb29ec91e
Pablo Fischer <pfischer@cpan.org>
2009-11-02 01:29:51 +0000

    Import of PFISCHER/Getopt-Awesome-0.1.1 from CPAN.
    gitpan-cpan-distribution: Getopt-Awesome
    gitpan-cpan-version:      0.1.1
    gitpan-cpan-path:         PFISCHER/Getopt-Awesome-0.1.1.tar.gz
    gitpan-cpan-author:       PFISCHER
    gitpan-cpan-maturity:     released

--- Getopt/Awesome/Examples/Foo/ChildFoo.pm
+++ Getopt/Awesome/Examples/Foo/ChildFoo.pm
@@ -1,12 +1,12 @@
 #
-# Foo::ChildFoo
+# Getopt::Awesome::Examples::Foo::ChildFoo
 #
 # Created: 10/07/2009 09:07:46 AM
-package Foo::ChildFoo;
+package Getopt::Awesome::Examples::Foo::ChildFoo;
 
 use strict;
 use warnings;
-use base qw(Foo);
+use base qw(Getopt::Awesome::Examples::Foo);
 use Getopt::Awesome qw(:common);
 
 define_option('example=s', 'An example from first child');
